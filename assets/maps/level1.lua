fbae0c376efa3b5938c7f3da805560fbdc590534
CKiilu <cndeti@gmail.com>
2016-09-18 18:37:06 +0300

    Added map level one

---
+++ assets/maps/level1.lua
@@ -0,0 +1,496 @@
+return {
+  version = "1.1",
+  luaversion = "5.1",
+  tiledversion = "0.14.2",
+  orientation = "orthogonal",
+  renderorder = "right-down",
+  width = 64,
+  height = 64,
+  tilewidth = 16,
+  tileheight = 16,
+  nextobjectid = 1,
+  properties = {},
+  tilesets = {
+    {
+      name = "tiles",
+      firstgid = 1,
+      tilewidth = 16,
+      tileheight = 16,
+      spacing = 0,
+      margin = 1,
+      image = "../../../SideScroller/assets/images/maps/sampleA.png",
+      imagewidth = 1365,
+      imageheight = 767,
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      properties = {},
+      terrains = {},
+      tilecount = 3995,
+      tiles = {}
+    },
+    {
+      name = "Backgrounds",
+      firstgid = 3996,
+      tilewidth = 1281,
+      tileheight = 720,
+      spacing = 0,
+      margin = 0,
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      properties = {},
+      terrains = {
+        {
+          name = "fsfs",
+          tile = -1,
+          properties = {}
+        },
+        {
+          name = "dfd",
+          tile = -1,
+          properties = {}
+        }
+      },
+      tilecount = 63,
+      tiles = {
+        {
+          id = 0,
+          image = "../../../SideScroller/assets/images/maps/PNG/backgrounds/bg4/bg4_a.png",
+          width = 1280,
+          height = 192
+        },
+        {
+          id = 1,
+          image = "../../../SideScroller/assets/images/maps/PNG/backgrounds/bg4/bg4_b.png",
+          width = 1280,
+          height = 293
+        },
+        {
+          id = 2,
+          image = "../../../SideScroller/assets/images/maps/PNG/backgrounds/bg4/bg4_c.png",
+          width = 1281,
+          height = 329
+        },
+        {
+          id = 3,
+          image = "../../../SideScroller/assets/images/maps/PNG/backgrounds/bg4/bg4_d.png",
+          width = 1280,
+          height = 359
+        },
+        {
+          id = 4,
+          image = "../../../SideScroller/assets/images/maps/PNG/backgrounds/bg4/bg4_e.png",
+          width = 1281,
+          height = 428
+        },
+        {
+          id = 5,
+          image = "../../../SideScroller/assets/images/maps/PNG/backgrounds/bg4/bg4_f.png",
+          width = 1081,
+          height = 77
+        },
+        {
+          id = 6,
+          image = "../../../SideScroller/assets/images/maps/PNG/backgrounds/bg4/bg4_g.png",
+          width = 1280,
+          height = 720
+        },
+        {
+          id = 7,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_03.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 8,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_05.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 9,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_07.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 10,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_09.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 11,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_11.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 12,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_13.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 13,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_15.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 14,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_24.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 15,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_25.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 16,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_26.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 17,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_27.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 18,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_28.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 19,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_29.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 20,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_30.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 21,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_38.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 22,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_39.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 23,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_40.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 24,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_41.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 25,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_42.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 26,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_43.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 27,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_44.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 28,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_52.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 29,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_53.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 30,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_54.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 31,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_55.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 32,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_56.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 33,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_57.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 34,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_58.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 35,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_66.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 36,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_67.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 37,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_68.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 38,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_69.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 39,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_70.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 40,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_71.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 41,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_72.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 42,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_80.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 43,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_81.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 44,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_82.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 45,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_83.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 46,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_84.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 47,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_85.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 48,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_86.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 49,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_94.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 50,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_95.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 51,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_96.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 52,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_97.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 53,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_98.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 54,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_99.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 55,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_100.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 56,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_108.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 57,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_109.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 58,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_110.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 59,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_111.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 60,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_112.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 61,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_113.png",
+          width = 128,
+          height = 128
+        },
+        {
+          id = 62,
+          image = "../../../SideScroller/assets/images/maps/PNG/grass_stone_tiles/grass_stone_114.png",
+          width = 128,
+          height = 128
+        }
+      }
+    }
+  },
+  layers = {
+    {
+      type = "tilelayer",
+      name = "BackGround",
+      x = 0,
+      y = 0,
+      width = 64,
+      height = 64,
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      properties = {
+        ["solid"] = "0"
+      },
+      encoding = "base64",
+      compression = "zlib",
+      data = "eJzt1EEKwyAQBdDsc//r1Kt1FUgHWzUpdorvQQi4kPkz6rYBAAAAAAAAAAAAAC2P/fUr+68rmuvIfPxXzV8Wyn8+6yvnj33Ifv9736mRTP+QvzarVqYrvcqSPdZ/ztFzT2u9erdntvf/U52xDyN7lEofSljPkj/O+c75b81+pK8z9J79b9WZ6d4DALCOJ4/ZeFw="
+    },
+    {
+      type = "tilelayer",
+      name = "ForeGround",
+      x = 0,
+      y = 0,
+      width = 64,
+      height = 64,
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      properties = {
+        ["solid"] = "0"
+      },
+      encoding = "base64",
+      compression = "zlib",
+      data = "eJzt0LkNACAIAEB69x9JXUtLK2NhIeYuoeIJEAEAAAAAAAAAAAAAAP+oJaLN6GVfUzf5bE5uvtn3gtu7r/My/wUAAIDcBt8jEFM="
+    },
+    {
+      type = "tilelayer",
+      name = "Solids",
+      x = 0,
+      y = 0,
+      width = 64,
+      height = 64,
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      properties = {
+        ["solid"] = "1"
+      },
+      encoding = "base64",
+      compression = "zlib",
+      data = "eJzt0jEOgCAQBEB6+YE/Ut/tu7QkEQIWxgAzCQ2huGUvBAAAAAAAAAAAAABos8UQ9vsc8e9J4Hs973tt9p6zlZQyncvzbXrX81+0zJ7LvzbsRc//kqr1P7o3/Y9o9v5zXc/U/0xZAQAYywW8IxB5"
+    }
+  }
+}
