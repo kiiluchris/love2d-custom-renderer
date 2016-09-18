9aa7c17abc56ed1a919f6a7f43174d2855cbc18b
CKiilu <cndeti@gmail.com>
2016-09-18 17:09:48 +0300

    Built a simple renderer

--- main.lua
+++ main.lua
@@ -1,8 +1,30 @@
+local Renderer = require("/tools/renderer")
 
+renderer = Renderer:create()
+
+function createBox(x, y, l)
+	local box = {
+		x = x or 0,
+		y = y or 0
+	}
+
+	function box:load()
+		renderer:addRenderer(self, l)
+	end
+
+	function box:draw()
+		love.graphics.setColor(math.random(0,255), math.random(0,255), math.random(0,255))
+		love.graphics.rectangle("fill", self.x, self.y, 64, 64)
+	end
+
+	return box
+end
 
 function love.load()
-
-
+	r1 = createBox(64,64)
+	r2 = createBox(96,96)
+	r1:load()
+	r2:load()
 end
 
 function love.update(dt)
@@ -11,6 +33,5 @@ function love.update(dt)
 end
 
 function love.draw()
-
-
+	renderer:draw()
 end
