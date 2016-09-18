8b49b6fce38afd2b43bd4ebfe0913d90c91a4054
CKiilu <cndeti@gmail.com>
2016-09-18 17:32:07 +0300

    Added basic movement module

--- main.lua
+++ main.lua
@@ -11,42 +11,9 @@ g_GameTime = 0
 
 
 
-function createBox(x, y, l)
-	local box = {
-		x = x or 0,
-		y = y or 0
-	}
-
-	function box:load()
-		renderer:addRenderer(self, l)
-	end
-
-	function box:draw()
-		love.graphics.setColor(math.random(0,255), math.random(0,255), math.random(0,255))
-		love.graphics.rectangle("fill", self.x, self.y, 64, 64)
-	end
-
-	return box
-end
-
-function updateTimer()
-	local ticker = {}
-
-	function ticker:load()
-		gameLoop:addGameLoop(self)
-
-	end
-
-	function ticker:tick( dt )
-		print(dt, math.random())
-	end
-
-	return ticker
-
-end
 function love.load()
-	obj = updateTimer()
-	obj:load()
+
+	
 end
 
 function love.update(dt)
